f2bce0714d07e9d62c57630aaf218f95a6d3b7a8
saqlain <shaikhsaqlain11@gmail.com>
2024-11-12 15:23:15 +0530

    chore: expense doctype

--- recaptcha/www/login_validation.py
+++ recaptcha/www/login_validation.py
@@ -2,22 +2,23 @@ import frappe
 import requests
 from frappe import _
 
-SECRET_KEY = '6LcfmQYqAAAAANoUgs2TDjyWdodz3lWYdT6fCc7z'
-
 @frappe.whitelist(allow_guest=True)
 def login_with_recaptcha():
     """
-    Custom login method with reCAPTCHA verification develop by saqlain shaikh.
+    Custom login method with reCAPTCHA verification.
     """
     data = frappe.local.form_dict
     username = data.get('usr')
     password = data.get('pwd')
     recaptcha_response = data.get('recaptcha_response')
 
+    # Fetch secret key from Recaptcha Settings
+    secret_key = frappe.db.get_single_value('Recaptcha Settings', 'secret_key')
+
     # Verify reCAPTCHA
     recaptcha_verify_url = 'https://www.google.com/recaptcha/api/siteverify'
     recaptcha_payload = {
-        'secret': SECRET_KEY,
+        'secret': secret_key,
         'response': recaptcha_response
     }
     recaptcha_result = requests.post(recaptcha_verify_url, data=recaptcha_payload).json()
